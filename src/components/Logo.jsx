6bb82503d1bc26dd8fe8c5d7e6054dd76c0b316c
Tatiana Garcia <t.dvla8549@gmail.com>
2024-08-11 09:19:26 -0500

    develop/Agregando Logo y mejorando el menu hamburguesa

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import "../styles/logo.scss";
+import logo from "../assets/img/icons/logo.svg"; // Adjust the path as necessary
 
 const Logo = () => {
-  return <div className="logo">loopstudios</div>;
+  return (
+    <div className="logo">
+      <img src={logo} alt="Loopstudios Logo" />
+    </div>
+  );
 };
 
 export default Logo;
