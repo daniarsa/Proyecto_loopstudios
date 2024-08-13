deb113481bb51849310c47431ed0139953b213f8
Daniela Araque Sanchez <Dani.a.sanchez1106@hotmail.com>
2024-08-13 15:00:22 -0500

    Actualizando rutas de imagenes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import SectionItem from './Section_infoItem';
 import Section_info from './Section_info';
-import deep from '../assets/img/desktop/image-deep-earth.jpg';
+import Deep_earth from '../assets/img/desktop/image-deep-earth.jpg';
+import Night_arcade from '../assets/img/desktop/image-night-arcade.jpg';
+import Soccer_team_vr from '../assets/img/desktop/image-soccer-team.jpg';
+import The_grid from '../assets/img/desktop/image-grid.jpg';
+import From_up_above_ve from '../assets/img/desktop/image-from-above.jpg';
+import Pocket_borealis from '../assets/img/desktop/image-pocket-borealis.jpg';
+import The_curiosity from '../assets/img/desktop/image-curiosity.jpg';
+import Make_it_fisheye from '../assets/img/desktop/image-fisheye.jpg';
 import "../styles/Main.scss";
 
 const creations = [
-  { title: 'DEEP EARTH', imageUrl: deep},
-  { title: 'NIGHT ARCADE', imageUrl: 'src/assets/img/desktop/image-night-arcade.jpg' },
-  { title: 'SOCCER TEAM VR', imageUrl: 'src/assets/img/desktop/image-soccer-team.jpg' },
-  { title: 'THE GRID', imageUrl: 'src/assets/img/desktop/image-grid.jpg' },
-  { title: 'FROM UP ABOVE VR', imageUrl: 'src/assets/img/desktop/image-from-above.jpg' },
-  { title: 'POCKET BOREALIS', imageUrl: 'src/assets/img/desktop/image-pocket-borealis.jpg' },
-  { title: 'THE CURIOSITY', imageUrl: 'src/assets/img/desktop/image-curiosity.jpg' },
-  { title: 'MAKE IT FISHEYE', imageUrl: 'src/assets/img/desktop/image-fisheye.jpg' },
+  { title: 'DEEP EARTH', imageUrl: Deep_earth},
+  { title: 'NIGHT ARCADE', imageUrl: Night_arcade },
+  { title: 'SOCCER TEAM VR', imageUrl: Soccer_team_vr },
+  { title: 'THE GRID', imageUrl: The_grid },
+  { title: 'FROM UP ABOVE VR', imageUrl: From_up_above_ve },
+  { title: 'POCKET BOREALIS', imageUrl: Pocket_borealis },
+  { title: 'THE CURIOSITY', imageUrl: The_curiosity  },
+  { title: 'MAKE IT FISHEYE', imageUrl: Make_it_fisheye },
 ];
 
 const Main = () => {
